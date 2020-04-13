242cc3baf38f417e9a9c7da1ce9b8aef9b8a115f
vmundra <krishnamundra91@gmail.com>
2020-04-13 15:02:22 +0530

    table already exits error came

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,11 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\User;
+use App\Category;
+use App\Product;
+use App\Transaction;
+use App\Seller;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,11 +18,58 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(User::class, function (Faker $faker) {
+    static $password;
+
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        // 'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+        'verified' =>$verified = $faker->randomElement([User::VERIFIED_USER, User::UNVERIFIED_USER]),
+        'verification_token' =>$verified ==User::VERIFIED_USER ? null : User::generateVerificationCode(),
+        'admin' =>$verified = $faker->randomElement([User::ADMIN_USER, User::REGULAR_USER]),
+
+    ];
+});
+
+
+
+$factory->define(Category::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        
+    ];
+});
+
+
+
+$factory->define(Product::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->numberBetween(1,10),
+        'status' => $faker->randomElement([Product::AVAILABLE_PRODUCT, Product::UNAVAILABLE_PRODUCT]),
+        'image' => $faker->randomElement(['1.jpg', '2.jpg', '3.jpg']),
+        'seller_id' => User::all()->random()->id,
+        
+    ];
+});
+
+
+
+$factory->define(Transaction::class, function (Faker\Generator $faker) {
+
+    $seller = Seller::has('products')->get()->random();
+    $buyer = User::all()->except($seller->id)->random();
+
+    return [
+    
+        'quantity' => $faker->numberBetween(1,3),
+        'buyer_id' => $buyer->id,
+        'product_id' => $seller->products->random()->id,
+        
     ];
 });
--- database/migrations/2020_04_12_211547_create_products_table.php
+++ database/migrations/2020_04_12_211547_create_products_table.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use App\Product;
+use App\User;
+use App\Seller;
 
 class CreateProductsTable extends Migration
 {
@@ -14,17 +16,18 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('product', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('description',1000);
-            $table->integer('quantity')->unsigned;
+            $table->integer('quantity')->unsigned();
             $table->string('status')->default(Product::UNAVAILABLE_PRODUCT);
             $table->string('image');
-            $table->integer('seller_id')->unsigned;
+            $table->integer('seller_id')->unsigned();
             $table->timestamps();
 
             $table->foreign('seller_id')->references('id')->on('users');
+            
         });
     }
 
